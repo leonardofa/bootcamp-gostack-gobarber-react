4b21d6abf40e91672734687e554936d2afcf47ce
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-02-19 00:33:24 -0300

    Exibindo repositório

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
+import Container from '../../components/Container';
+import { Loading, Owner } from './styles';
+
 export default class Repository extends Component {
-  state = { repository: '', issues: '', loading: true };
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = { repository: {}, issues: [], loading: true };
 
   async componentDidMount() {
     const { match } = this.props;
@@ -19,17 +32,27 @@ export default class Repository extends Component {
       }),
     ]);
 
-    this.setState({ repository: repository.data, issues: issues.data });
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
   }
 
   render() {
     const { repository, issues, loading } = this.state;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
     return (
-      <>
-        <h1>Repository: {repository}</h1>
-        <h1>Issues: {issues}</h1>
-        <h1>Loading: {loading}</h1>
-      </>
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
     );
   }
 }
