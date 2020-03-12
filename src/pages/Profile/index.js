34778d3426b211616ac4e1936ce3c1d4f879ad47
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-03-12 19:28:58 -0300

    Foto de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,7 @@ import { Form, Input } from '@rocketseat/unform';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
+import AvatarInput from './AvatarInput';
 
 export default function Profile() {
   const dispatch = useDispatch();
@@ -18,6 +19,7 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 
